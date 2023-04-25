2e09208a36043da5a2dbddad0ea53f0402a7ef6c
Vivek Poddar <vivekimsit@gmail.com>
2023-04-25 15:18:10 +0530

    Test.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: 'https://sys-design-course.vercel.app',
   experimental: {
     scrollRestoration: true,
   },
