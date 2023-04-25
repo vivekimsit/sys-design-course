b3a459741f4e2e79a5fc2c4ad5952a6992c49194
Vivek Poddar <vivekimsit@gmail.com>
2023-04-25 16:16:49 +0530

    Undo.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const basePath =
-  process.env.NODE_ENV === 'production'
-    ? 'https://sys-design-course.vercel.app'
-    : ''
-
 const nextConfig = {
-  basePath,
   reactStrictMode: true,
   // assetPrefix: 'https://sys-design-course.vercel.app',
   experimental: {
